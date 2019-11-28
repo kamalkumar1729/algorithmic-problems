4db1fb0ae7a405cb306863ad9c897d02a0699339
Kamal Kumar <kamal7.kumar@ril.com>
2019-11-28 22:29:06 +0530

    tail recursive addNumbers

--- src/main/java/recursion/AddNumbers.java
+++ src/main/java/recursion/AddNumbers.java
@@ -1,8 +1,13 @@
 package recursion;
 
 public class AddNumbers {
+
+    private static Integer count = 0;
+
     public static void main(String[] args) {
-        System.out.println(addNumbers(5,-7));
+        //System.out.println(addNumbersTailRecursive(100000,100,0));
+        System.out.println(addNumbers(100000,100));
+        System.out.println(count);
     }
 
     private static int addNumbers(int a, int b) {
@@ -10,5 +15,15 @@ public class AddNumbers {
         if (b==0) return a;
         return addNumbers(a-1,b-1)+2;
     }
+
+    /*tail recursive program */
+    private static int addNumbersTailRecursive(int a, int b,int res) {
+        count++;
+        if (a==0) return res+b;
+        else if (b==0) return res+a;
+        else return addNumbersTailRecursive(a-1,b-1,res+2);
+    }
+
+
 
 }
